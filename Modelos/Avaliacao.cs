304fa59f2879fe253675734780f5918260c82759
Gabriel Freitas <freitasgabb7@gmail.com>
2024-08-15 22:28:35 -0300

    Add files via upload

---
+++ Modelos/Avaliacao.cs
@@ -0,0 +1,16 @@
+﻿namespace ScreenSound3.Modelos;
+
+internal class Avaliacao
+{
+    public Avaliacao(int nota)
+    {
+        //condicao aqui
+        Nota = nota;
+    }
+    public int Nota { get; }
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
+}
